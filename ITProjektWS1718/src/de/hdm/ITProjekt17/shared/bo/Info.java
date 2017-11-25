01846437a83d9a1e05b291eefada4f155cef0d55
DennisLehle <DennisLehle@10.60.60.41>
2017-11-25 03:12:39 +0100

    extended class "Info"

--- ITProjektWS1718/src/de/hdm/ITProjekt17/shared/bo/Info.java
+++ ITProjektWS1718/src/de/hdm/ITProjekt17/shared/bo/Info.java
@@ -1,5 +1,71 @@
 package de.hdm.ITProjekt17.shared.bo;
 
+
+
+/**
+ * Realisierung eines exemplarischen Info-Objektes für das Profil
+ * 
+ * @author dezzyanthony
+ *
+ */
+
 public class Info extends BusinessObject{
 
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	//-------------------Variablen-der-Info-Klasse--------------------
+	
+	/**
+	 * Variable profilId ist dei Id des Profil's
+	 */
+	private int profilId = 0;
+	
+	/**
+	 * Variable text enthält informationen des Profils
+	 */
+	private String text = "";
+	
+	
+	
+	//-----------------------Get-Methoden-----------------------------
+	
+	/**
+	 * Auslesen der PrfilId
+	 * @return
+	 */
+	public int getProfilId(){
+		return profilId;
+	}
+	
+	/**
+	 * Auslesen der Information eines Profils
+	 * @return
+	 */
+	public String getText(){
+		return text;
+	}
+	
+	
+	
+	//------------------------Set-Methoden---------------------------
+	
+	/**
+	 * Setzen einer ProfilId
+	 * @param profilId
+	 */
+	public void setProfilId(int profilId){
+		this.profilId = profilId;
+	}
+	
+	/**
+	 * Setzen eines Texte's
+	 * @param text
+	 */
+	public void setText(String text){
+		this.text = text;
+	}
+	
+	
 }

e26b3d87cf0f298376f2313a00b381915880a93a
DennisLehle <DennisLehle@10.60.60.41>
2017-11-25 02:52:22 +0100

    extended class "Kontaktsperre" and "Merkzettel"

--- ITProjektWS1718/src/de/hdm/ITProjekt17/shared/bo/Kontaktsperre.java
+++ ITProjektWS1718/src/de/hdm/ITProjekt17/shared/bo/Kontaktsperre.java
@@ -1,5 +1,69 @@
 package de.hdm.ITProjekt17.shared.bo;
 
+/**
+ * Mit diseer Klasse werden exemplarische Kontaktsperren vergeben.
+ * Es ist möglich Teilnehmer, welche ungewünscht sind zu sperren.
+ * @author dezzyanthony
+ *
+ */
+
+
 public class Kontaktsperre extends BusinessObject {
+	
+	private static final long serialVersionUID = 1L; 
+
+	
+	//--------Variablen-der-Klasse-Kontaktsperre----------------
+	
+	/**
+	 * ID, welche vergeben wird wenn ein Teilnehmer gesperrt wurde
+	 */
+	private int kontaktsperreId = 0;
+	
+	/**
+	 * ID des Profils, welches gesperrt werden soll.
+	 */
+	private int profilId = 0;
+	
+	
+	//-------------------Get-Methoden----------------------------
+	
+	/**
+	 * Auslesen des Profils, welches gesperrt wurden.
+	 * @return
+	 */
+	public int getKontaktsperreId(){
+		return kontaktsperreId;
+	}
+	
+	/**
+	 * Auslesen des Profils, welches einen Kontakt gesperrt hat.
+	 * @return
+	 */
+	public int getProfilId(){
+		return profilId;
+	}
+	
+	
+	//-------------------Set-Methoden----------------------------
+	
+	/**
+	 * Setzen einer ID für das zu sperrende Profil
+	 * @param kontaktsperreId
+	 */
+	public void setKontaktsperreId(int kontaktsperreId){
+		this.kontaktsperreId = kontaktsperreId;
+	}
+	
+	/**
+	 * Setzen einer ID des Profils, welches die Sperrung durchführt
+	 * @param profilId
+	 */
+	public void setProfilId(int profilId){
+		this.profilId = profilId;
+	}
+	
+	//Wirklich nötig eine neue ProfilID zusetzen um ein Sperrendes Profil zu verwalten
+	
 
 }
--- ITProjektWS1718/src/de/hdm/ITProjekt17/shared/bo/Merkzettel.java
+++ ITProjektWS1718/src/de/hdm/ITProjekt17/shared/bo/Merkzettel.java
@@ -1,5 +1,63 @@
 package de.hdm.ITProjekt17.shared.bo;
 
+
+/**
+ * Darstellung einer exemplarischen Merkliste zum auflisten alle Teilnehmer der Partnerbörse,
+ * welche der Teilnehmern später kontaktieren möchte.
+ * 
+ * @author dezzyanthony
+ *
+ */
+
 public class Merkzettel extends BusinessObject{
+	
+	//---------------Variablen-der-Klasse-Merkzettel---------------
+	
+	
+	/**
+	 * Varaible des zumerkenden Profils
+	 */
+	private int merkzettelId = 0;
+	
+	/**
+	 * Variable von Profil, welche vermerk verhängt
+	 */
+	private int profilId = 0;
+	
+	
+	//------------------------Get-Methoden-------------------------
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getMerkzettelId(){
+		return merkzettelId;
+	}
+	
+	public int getProfilId(){
+		return profilId;
+	}
+	
+	//------------------------Set-Methoden-------------------------
+	
+	
+	/**
+	 * Gibt es pro Profil nur eine MerkzettelID???
+	 * @param merkId
+	 */
+	public void setMerkzettelId(int merkId){
+		merkzettelId = merkId;
+	}
+	
+	/**
+	 * Wird hier die ProfilID neu vergeben oder vergeben für das Profil, welches die Merkung durchführt??
+	 * Wenn es eine MerkzettelId pro Profil gibt wird die ProfilId der Klasse Profil hier übertragen oder neu angelegt?
+	 * @param profilId
+	 */
+	public void setProfilId(int profilId){
+		this.profilId = profilId;
+	}
+	
 
 }

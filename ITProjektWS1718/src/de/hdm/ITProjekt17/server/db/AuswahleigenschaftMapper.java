d398549fbf89ad7841534a743940fe3bdd4bf637
SaminaAhmed <sa056@hdm-stuttgart.de>
2017-12-02 20:49:25 +0100

    AuswahleigenschaftMapper angelegt

--- ITProjektWS1718/src/de/hdm/ITProjekt17/server/db/AuswahleigenschaftMapper.java
+++ ITProjektWS1718/src/de/hdm/ITProjekt17/server/db/AuswahleigenschaftMapper.java
@@ -4,8 +4,12 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import de.hdm.ITProjekt17.shared.bo.Auswahleigenschaft;
+import de.hdm.ITProjekt17.shared.bo.Eigenschaft;
+
+
 
 public class AuswahleigenschaftMapper {
 
@@ -37,6 +41,65 @@ public class AuswahleigenschaftMapper {
 			
 			return auswahleigenschaftMapper;
 		}
+		
+		public ArrayList<Auswahleigenschaft> insertAuswahleigenschaft(ArrayList<Auswahleigenschaft> aus) throws Exception {//c f�r character
+			Connection con = DBConnection.connection();
+
+			try {
+				Statement stmt = con.createStatement();
+				for(Auswahleigenschaft c : aus){
+					
+					ResultSet rs = stmt.executeQuery("SELECT MAX(`id`) AS maxid FROM auswahleigenschaft");
+					
+					if (rs.next()) {
+						/*
+						 * c erhält den bisher maximalen, nun um 1 inkrementierten
+						 * Primärschlüssel.
+						 */
+						c.setId(rs.getInt("maxid") + 1);
+						
+						stmt.executeUpdate("INSERT INTO `auswahleigenschaft` (`id`, `Wert`, `eigenschaftid`) VALUES ("+c.getId()+"', '"+c.getWert()+"', '"+c.getEigenschaftid()+"',);");
+						
+				}
+		
+				}
+			}catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return aus;
+		}
+		
+		public Auswahleigenschaft updateAuswahleigenschaft(Auswahleigenschaft aus) throws Exception {
+			Connection con = DBConnection.connection();
+
+			try {
+				Statement stmt = con.createStatement();
+
+				stmt.executeUpdate("UPDATE `auswahleigenschaft` SET `id` = '"+aus.getId()+"', `wert` = '"+aus.getWert()+"', `eigenschaftid` = '"+aus.getEigenschaftid()+"' WHERE `auswahleigenschaft`.`id` = "+aus.getId());
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+			return aus;
+		}
+		
+		public void deleteAuswahleigenschaft(Auswahleigenschaft aus) throws Exception {
+		    Connection con = DBConnection.connection();
+
+		    try {
+		      Statement stmt = con.createStatement();
+
+		      stmt.executeUpdate("DELETE FROM `auswahleigenschaft` WHERE `id` = "+aus.getId());
+		    }
+		    catch (SQLException e) {
+		      e.printStackTrace();
+		    }
+		  }
+					
+					
+					
+					
 		public Auswahleigenschaft findByKey(int id){
 			Connection con = DBConnection.connection();
 			
@@ -60,6 +123,54 @@ public class AuswahleigenschaftMapper {
 			return null;
 		}
 		
+		
+		public ArrayList<Auswahleigenschaft> getAll() throws Exception{
+			
+			Connection con = DBConnection.connection();
+			
+			ArrayList<Auswahleigenschaft> result = new ArrayList<Auswahleigenschaft>();
+			try {
+				Statement stmt = con.createStatement();
+
+				/*
+				 * Zunächst schauen wir nach, welches der momentan höchste
+				 * Primärschlüsselwert ist.
+				 */
+				ResultSet rs = stmt.executeQuery("SELECT * FROM `auswahleigenschaft`");
+
+				// Wenn wir etwas zurückerhalten, kann dies nur einzeilig sein
+				//if (rs.next()) {
+					/*
+					 * c erhält den bisher maximalen, nun um 1 inkrementierten
+					 * Primärschlüssel.
+					 */
+				//	a.setId(rs.getInt("") + 1);
+					while (rs.next()) {
+				          Auswahleigenschaft c = new Auswahleigenschaft();//default Konstruktor in Eigenscgaft.java einf�gen damit es kein Fehler anzeigt
+				          c.setId(rs.getInt("id"));
+				          c.setWert(rs.getString("Wert"));
+				          Eigenschaft e = new Eigenschaft();
+						  e.setId(rs.getInt("eigenschaft_id"));
+						  c.setEigenschaft(e);
+							// a.setId(rs.getInt("") + 1);
+							result.add(c);
+						}
+
+					stmt = con.createStatement();
+
+					// Jetzt erst erfolgt die tatsächliche Einfügeoperation
+//					stmt.executeUpdate("");
+//					return result;
+			//}
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return result;
+
+		
+		
 	}
+}
 
 

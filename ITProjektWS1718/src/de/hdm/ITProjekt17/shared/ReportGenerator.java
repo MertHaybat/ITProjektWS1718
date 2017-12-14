47946884b6d2c21ae3cc4172591f589de328901d
Dennis Lehle <dl043@hdm-stuttgart.de>
2017-12-14 17:52:39 +0100

    ReportGenerator Interface plus Async angelegt

---
+++ ITProjektWS1718/src/de/hdm/ITProjekt17/shared/ReportGenerator.java
@@ -0,0 +1,14 @@
+package de.hdm.ITProjekt17.shared;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+
+/**
+ * Synchrones Interface für RPC fähige Klasse von Reports.
+ * Gleiche Realisierungsgrundlage wie PartnerboerseAdministrationImpl, PartnerboerseAdrministrationAsync und PartnerboerseAdrministration.
+ * ReportGenerator erstellt berichte für den User / Ähnlichkeitsmaß der Partnerprofile.
+ * @author Thies
+ *
+ */
+public interface ReportGenerator extends RemoteService {
+
+}
---
+++ ITProjektWS1718/src/de/hdm/ITProjekt17/shared/ReportGeneratorAsync.java
@@ -0,0 +1,12 @@
+package de.hdm.ITProjekt17.shared;
+
+/**
+ * Das Gegenstück zu Synchronen Interface ReportGenerator.
+ * Wird vom Google Plugin semiautomatisch erstellt und verwalter/gepflegt.
+ * 
+ * @author Dennis Lehle
+ *
+ */
+public interface ReportGeneratorAsync {
+
+}

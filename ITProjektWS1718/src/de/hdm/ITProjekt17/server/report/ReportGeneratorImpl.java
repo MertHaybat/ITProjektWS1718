c2609486edb2d157d4306918be314cab0d26d9e5
Dennis Lehle <dl043@hdm-stuttgart.de>
2017-12-14 17:50:56 +0100

    ReportGeneratorImpl angelegt

---
+++ ITProjektWS1718/src/de/hdm/ITProjekt17/server/report/ReportGeneratorImpl.java
@@ -0,0 +1,49 @@
+package de.hdm.ITProjekt17.server.report;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+import de.hdm.ITProjekt17.server.PartnerboerseAdministrationImpl;
+import de.hdm.ITProjekt17.shared.PartnerboerseAdministration;
+import de.hdm.ITProjekt17.shared.ReportGenerator;
+import de.hdm.ITProjekt17.shared.bo.*;
+import de.hdm.ITProjekt17.shared.report.*;
+
+/**
+ * Implementierung des ReportGenerator Interfaces.
+ * Technische Relaisierung wie bei PartnerboerseAdministrationImpl (RPC-Fähigkeit für Übermittlung zwischen CLient und Server)
+ * @author Dennis Lehle
+ *
+ */
+public class ReportGeneratorImpl extends RemoteServiceServlet implements ReportGenerator{
+	
+	/**
+	 * Der ReportGenerator benötigt Zugriff auf die PartnerboerseAdministrations Klasse,
+	 * weil diese den Zugriff auf die BusinesObjekte hat.
+	 */
+	private PartnerboerseAdministration partnerboerseadministration = null;
+	
+	/**
+	 * Da ein RemoteServiceServlet durch GWT GWT.create(Klassenname.class) CLient-Seitig erstellt wird.
+	 * Dazu wird ein No Argument Constructor erstellt und nach hinein eine extra Instanz die Client-Seitig nach 
+	 * GWT.create(Klassenname.class) aufgerufen wird.
+	 * 
+	 * @throws IllegalArgumentException
+	 */
+	public ReportGeneratorImpl() throws IllegalArgumentException {
+	  }
+	
+	/**
+	 * Initialisierungemethode -> Schau Erklärung ReportGeneratorImpl.
+	 */
+	public void init() throws IllegalArgumentException {
+	    /*
+	     * Ein ReportGeneratorImpl-Objekt instantiiert für seinen Eigenbedarf eine
+	     * PartnerboerseAdministrationImpl-Instanz.
+	     */
+	    PartnerboerseAdministrationImpl a = new PartnerboerseAdministrationImpl();
+	    a.init();
+	    this.partnerboerseadministration = a;
+	  }
+
+
+}

08ebd5f50973e149a5262e5d93cf027fe8396e75
Giuseppe Galati <gg025@hdm-stuttgart.de>
2018-01-04 19:36:08 +0100

    Startseite angepasst

--- ITProjektWS1718/src/de/hdm/ITProjekt17/client/gui/Startseite.java
+++ ITProjektWS1718/src/de/hdm/ITProjekt17/client/gui/Startseite.java
@@ -1,32 +1,32 @@
 package de.hdm.ITProjekt17.client.gui;
 
-import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Startseite extends VerticalPanel{
 	
+	private Impressum impressum = new Impressum();
 	private VerticalPanel vpanel = new VerticalPanel();
 	private FlexTable ft1 = new FlexTable();
-	private Anchor an1 =new Anchor("Text"); 
 	private HTML html1 = new HTML("<article> <h2>Die kostenlose Partnerbörse</h2> "
 				+ "<section>"
 				+ "<h3>Finde die Liebe deines Lebens</h3> "
 				+ "<p>"
 				+ "Herzlich Willkommen auf der Partnerbörse </br>"
-				+ "Hier kannst du nach Herzenslust mit anderen Mitgliedern flirten , dich zum Dating und Ausgehen verabreden oder andere Singles mit den gleichen Interessen kennen lernen."
+				+ "Hier kannst du nach Herzenslust mit anderen Mitgliedern flirten ,dich zum Dating und Ausgehen verabreden oder andere Singles mit den gleichen Interessen kennen lernen.</br>"
 				+ "Es warten viele spannende und neue Bekanntschaften auf dich.</br>"
 				+ "Viel Spaß und viel Glück.</br> "
+				+ "<p>"
 				+ "Falls du Fragen zur Funktion hast oder Support benötigst, kannst du unsere Kontaktdaten im Impressum finden. </p>"
 				+ "	</section>	"
 				+ "</article>");
-	
-	
+
 	
 	public Startseite(){
-		ft1.setWidget(0,0,an1);
+		
 		ft1.setWidget(0, 1, html1);
+
 		
 		vpanel.add(ft1);
 		this.add(vpanel);

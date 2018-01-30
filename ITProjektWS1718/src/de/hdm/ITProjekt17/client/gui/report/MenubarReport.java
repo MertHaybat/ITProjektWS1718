9ca5d050c7d89e8da0ead21808731291a150d127
Fatjona Mustafi <fm052@hdm-stuttgart.de>
2018-01-30 17:42:15 +0100

    ReportGenerator - Klassen angelegt für Seitenerstellung.

---
+++ ITProjektWS1718/src/de/hdm/ITProjekt17/client/gui/report/MenubarReport.java
@@ -0,0 +1,97 @@
+package de.hdm.ITProjekt17.client.gui.report;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import de.hdm.ITProjekt17.shared.bo.Profil;
+
+public class MenubarReport extends VerticalPanel { 
+	
+	private VerticalPanel vpanel = new VerticalPanel();
+	private Button btn1 = new Button("StartseiteReport");
+	private Button btn2 = new Button("Report aller Profile");
+	private Button btn3 = new Button("Report aller unbesuchter Profile");
+	private Button btn4 = new Button("Report aller Profile ähnlich der Suchprofile");
+	
+	
+	private FlexTable ft1 = new FlexTable();
+	public MenubarReport(){
+		btn1.setStylePrimaryName("menubarbutton");
+		btn2.setStylePrimaryName("menubarbutton");
+		btn3.setStylePrimaryName("menubarbutton");
+		btn4.setStylePrimaryName("menubarbutton");
+		
+		ft1.setWidget(0, 0, btn1);
+		ft1.setWidget(1, 0, btn2);
+		ft1.setWidget(2, 0, btn3);
+		ft1.setWidget(3, 0, btn4);
+		
+
+		vpanel.add(ft1);
+		this.add(vpanel);
+		
+	}
+	
+	public MenubarReport(final Profil profil){
+		btn1.setStylePrimaryName("menubarbutton");
+		btn2.setStylePrimaryName("menubarbutton");
+		btn3.setStylePrimaryName("menubarbutton");
+		btn4.setStylePrimaryName("menubarbutton");
+		
+		ft1.setWidget(0, 0, btn1);
+		ft1.setWidget(1, 0, btn2);
+		ft1.setWidget(2, 0, btn3);
+		ft1.setWidget(3, 0, btn4);
+
+		
+		btn1.addClickHandler(new ClickHandler(){
+
+			@Override
+			public void onClick(ClickEvent event) {
+			RootPanel.get("Details").clear();
+			RootPanel.get("Details").add(new StartseiteReport(profil));
+			}
+			
+		});
+		
+		btn2.addClickHandler(new ClickHandler(){
+			@Override
+			public void onClick(ClickEvent event) {
+
+				ReportOfAllProfilesSeite p = new ReportOfAllProfilesSeite();
+				RootPanel.get("Details").clear();
+				RootPanel.get("Details").add(p);
+			}
+		});
+
+		btn3.addClickHandler(new ClickHandler(){
+
+			@Override
+			public void onClick(ClickEvent event) {
+				RootPanel.get("Details").clear();
+				RootPanel.get("Details").add(new ReportOfAllUnbesuchteProfilesByAehnlichkeitSeite());
+			}
+			
+		});
+		
+		btn4.addClickHandler(new ClickHandler(){
+
+			@Override
+			public void onClick(ClickEvent event) {
+				RootPanel.get("Details").clear();
+				RootPanel.get("Details").add(new ReportOfAllAehnlicheProfilesBySuchprofilesSeite());
+			}
+			
+		});
+		
+		vpanel.add(ft1);
+		this.add(vpanel);
+		
+	}
+
+}
--- ITProjektWS1718/src/de/hdm/ITProjekt17/client/gui/report/StartseiteReport.java
+++ ITProjektWS1718/src/de/hdm/ITProjekt17/client/gui/report/StartseiteReport.java
@@ -1,9 +1,40 @@
 package de.hdm.ITProjekt17.client.gui.report;
 
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.VerticalPanel;
+
+import de.hdm.ITProjekt17.shared.bo.Profil;
 
 public class StartseiteReport extends VerticalPanel{
 	
+	private VerticalPanel vpanel = new VerticalPanel();
+	private FlexTable ft1 = new FlexTable();
+	private HTML html1 = new HTML("<article> <h2>Report der Partnerbörse</h2> "
+				+ "<section>"
+				+ "<h3>Ausgabe der Reports</h3> "
+				+ "<p>"
+				+ "Herzlich Willkommen auf der Report-Startseite der Partnerbörse </br>"
+				+ "Hier kannst du deine Reports für verschiedene Anforderungen ausgeben lassen.</br>"
+				+ "Dabei bestehen die Möglichkeiten, drei Reports nach Belieben ausgeben zu lassen.</br>"
+				+ "Zu diesen Reports gehören:</br>"
+				+ "- die Ausgabe aller Profile.</br>"
+				+ "- die Ausgabe aller unbesuchten Profile, die dem eigenen Profil ähnlich sind.</br>"
+				+ "- die Ausgabe aller Profile, die den angelegten Suchprofilen ähnlich sind."
+				+ "<p>"
+				+ "Falls du Fragen zur Funktion hast oder Support benötigst, kannst du unsere Kontaktdaten im Impressum finden. </p>"
+				+ "	</section>"
+				+ "</article>");
+
 	
+	public StartseiteReport(final Profil profil){
+		
+		ft1.setWidget(0, 1, html1);
+
+		
+		vpanel.add(ft1);
+		this.add(vpanel);
+		
+	}
 
 }
